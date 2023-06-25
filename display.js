7379a7ec2f81743a1f081f43a6e053622d2767d0
Rahul Chauhan <60099783+dev-rc@users.noreply.github.com>
2023-06-26 02:55:22 +0530

    Add files via upload

--- display.js
+++ display.js
@@ -4,28 +4,64 @@ var correct = 0;
 var userInput;
 var answer;
 var timer;
-var timercount = 10;
+var timercount = 30;
 var answerString;
-var level = false;
+var levelNameState = "beginner";
+var levelDigitValue = 10;
+
+var levelname = document.getElementsByName("level");
+var beginner = document.getElementById("bgl");
+var intermediate = document.getElementById("inl");
+var advance = document.getElementById("adl");
+beginner.checked = true;
+
 alert("Click okay to start your Practise");
-generateQuestion();
+
 
 function clearTextFeild() {
     document.getElementById("answer-id").value = null;
 }
-var beginner = document.getElementById("bgl");
-var inter = document.getElementById("iml");
-var advc = document.getElementById("adl");
 
-if (beginner.check == true) {
-    beginner.onclick = function () {
-        console.log("1");
-    }
-} else if (inter.check == true) {
-    console.log("2");
-} else if (advc.check == true) {
-    console.log("3");
+
+beginner.onclick = function (){
+   
+    if (beginner.checked == true) {
+       alert("Beginner Level");
+       levelDigitValue = 10;
+       clearTimeout(timer);
+       setTimeout(generateQuestion, 1000); 
+      // generateQuestion();         
+    }   
 }
+
+intermediate.onclick = function (){
+    
+   
+    
+    if (intermediate.checked == true) {       
+            alert("Intermediate Level");
+            levelDigitValue = 100;
+            clearTimeout(timer);
+            setTimeout(generateQuestion, 1000);
+          //  generateQuestion();
+    } 
+   
+}
+advance.onclick = function (){
+   
+   
+    if (advance.checked == true) {
+            alert("Advance Level");
+            levelDigitValue = 1000;
+            clearTimeout(timer);
+            setTimeout(generateQuestion, 1000);  
+           // generateQuestion();
+    }   
+}
+
+//GENERATE QUESTION / START GAME
+
+generateQuestion();
 
 function startTimer() {
 
@@ -39,25 +75,39 @@ function startTimer() {
         clearTimeout(timer);
         document.getElementById("timer").innerHTML = "TIME OUT";
         clearTextFeild();
-        setTimeout(generateQuestion, 2000);
+        setTimeout(generateQuestion, 1000);
     }
 }
 
-document.getElementById("bgl").check = true;
+
 
 function generateQuestion() {
-
-    a = Math.floor(Math.random() * 100) + 1;
-    b = Math.floor(Math.random() * 100) + 1;
+    var numberDigits = levelDigitValue;
+    a = Math.floor(Math.random() * numberDigits) + 1;
+    b = Math.floor(Math.random() * numberDigits) + 1;
     answer = a + b;
     document.getElementById("question-id").innerHTML = "" + a + " + " + b;
     document.getElementById("image-id").src = "mark.png";
     document.getElementById("answer-display").innerText = "Find";
     document.getElementById("answer-id").focus();
-    timercount = 10;
-    startTimer();
-}
 
+    if(beginner.checked == true){
+
+        timercount = 30;
+        startTimer();
+    }
+    if(intermediate.checked == true){
+
+        timercount = 20;
+        startTimer();
+    }
+    if(advance.checked == true){
+
+        timercount = 10
+        startTimer();
+    }
+    
+}
 
 
 
@@ -73,27 +123,33 @@ function displayForWrongAnswer() {
     document.getElementById("image-id").src = "wrong.png";
     document.getElementById("wrong-ans").innerHTML = wrong = wrong + 1;
 }
+
 function check() {
     userInput = document.getElementById("answer-id").value;
 
-    if (userInput != 0) {
+    if (userInput != null) {
 
         if (userInput == answer) {
             displayForRightAnswer();
             clearTimeout(timer);
+            setTimeout(generateQuestion, 1000);
+            clearTextFeild();
         }
         else {
             displayForWrongAnswer();
             clearTimeout(timer);
+            setTimeout(generateQuestion, 1000);
+            clearTextFeild();
         }
     }
     else {
         document.getElementById("image-id").src = "tenor.gif";
         clearTimeout(timer);
-
+        setTimeout(generateQuestion, 1000);
+        clearTextFeild();
     }
     clearTextFeild();
-    setTimeout(generateQuestion, 2000);
+   
 }
 document.getElementById("btn").onclick = function () {
     check();
